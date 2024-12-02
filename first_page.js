e36f64bb091321bfd7392efc0d4fe70ec75a4c8f
sagecub760 <811987@seq.org>
2024-12-02 12:27:28 -0800

    First draft, added first and second page

---
+++ first_page.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+    // Trigger the swipe in animation when the document is ready
+    $("#Family").animate({
+        left: "0", // Move div to its original position (0)
+        opacity: 1  // Fade in
+    }, 1000); // Duration of 1 second
+});
+$(document).ready(function() {
+    // Trigger the swipe in animation when the document is ready
+    $("#Mom").animate({
+        left: "0", // Move div to its original position (0)
+        opacity: 1  // Fade in
+    }, 1000); // Duration of 1 second
+});
+$(document).ready(function() {
+    // Trigger the swipe in animation when the document is ready
+    $("#Dad").animate({
+        left: "0", // Move div to its original position (0)
+        opacity: 1  // Fade in
+    }, 1000); // Duration of 1 second
+});
+$(document).ready(function() {
+    // Trigger the swipe in animation when the document is ready
+    $("#Brother").animate({
+        left: "0", // Move div to its original position (0)
+        opacity: 1  // Fade in
+    }, 1000); // Duration of 1 second
+});
